8b2dfdbc623cd47bb992cb88c746924edc132cc1
divyabilthare <95240378+divyabilthare@users.noreply.github.com>
2023-12-12 12:21:49 +0530

    changes html,css,js

--- script.js
+++ script.js
@@ -28,3 +28,18 @@ tl.to("h5",{
     duration:0.7,
     yoyo:true
 })
+
+
+
+gsap.set(".information", { yPercent: 100 });
+
+gsap.utils.toArray(".container").forEach((container) => {
+  let info = container.querySelector(".information"),
+    silhouette = container.querySelector(".silhouette .cover"),
+    tl = gsap.timeline({ paused: true });
+
+  tl.to(info, { yPercent: 0 }).to(silhouette, { opacity: 0 }, 0);
+
+  container.addEventListener("mouseenter", () => tl.timeScale(1).play());
+  container.addEventListener("mouseleave", () => tl.timeScale(3).reverse());
+});
